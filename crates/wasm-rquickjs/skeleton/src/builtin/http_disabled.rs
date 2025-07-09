ed6c8ca94d183b754bf1f1161c2da81b45d4f0a5
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2025-07-09 15:19:27 +0200

    Fix

--- crates/wasm-rquickjs/skeleton/src/builtin/http_disabled.rs
+++ crates/wasm-rquickjs/skeleton/src/builtin/http_disabled.rs
@@ -1,8 +1,8 @@
 #[rquickjs::module]
 pub mod native_module {}
 
-// JS functions for the console implementation
 pub const HTTP_JS: &str = "";
+pub const FETCH_BLOB_JS: &str = "";
+pub const FORMDATA_JS: &str = "";
 
-// JS code wiring the console module into the global context
 pub const WIRE_JS: &str = "";
