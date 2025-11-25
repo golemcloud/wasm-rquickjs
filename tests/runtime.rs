c80db13216a8c18023b4d9f8631af711c3f3c459
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2025-11-25 13:17:43 +0100

    Format

--- tests/runtime.rs
+++ tests/runtime.rs
@@ -1243,8 +1243,11 @@ fn compiled_path() -> CompiledTest {
 }
 
 #[test]
-async fn path_test_basename(#[tagged_as("path")] compiled_test: &CompiledTest) -> anyhow::Result<()> {
-    let (r, output) = invoke_and_capture_output(compiled_test.wasm_path(), None, "test-basename", &[]).await;
+async fn path_test_basename(
+    #[tagged_as("path")] compiled_test: &CompiledTest,
+) -> anyhow::Result<()> {
+    let (r, output) =
+        invoke_and_capture_output(compiled_test.wasm_path(), None, "test-basename", &[]).await;
     let r = r?;
     println!("Output:\n{}", output);
     assert_eq!(r, Some(Val::Bool(true)));
@@ -1252,8 +1255,11 @@ async fn path_test_basename(#[tagged_as("path")] compiled_test: &CompiledTest) -
 }
 
 #[test]
-async fn path_test_dirname(#[tagged_as("path")] compiled_test: &CompiledTest) -> anyhow::Result<()> {
-    let (r, output) = invoke_and_capture_output(compiled_test.wasm_path(), None, "test-dirname", &[]).await;
+async fn path_test_dirname(
+    #[tagged_as("path")] compiled_test: &CompiledTest,
+) -> anyhow::Result<()> {
+    let (r, output) =
+        invoke_and_capture_output(compiled_test.wasm_path(), None, "test-dirname", &[]).await;
     let r = r?;
     println!("Output:\n{}", output);
     assert_eq!(r, Some(Val::Bool(true)));
@@ -1261,8 +1267,11 @@ async fn path_test_dirname(#[tagged_as("path")] compiled_test: &CompiledTest) ->
 }
 
 #[test]
-async fn path_test_extname(#[tagged_as("path")] compiled_test: &CompiledTest) -> anyhow::Result<()> {
-    let (r, output) = invoke_and_capture_output(compiled_test.wasm_path(), None, "test-extname", &[]).await;
+async fn path_test_extname(
+    #[tagged_as("path")] compiled_test: &CompiledTest,
+) -> anyhow::Result<()> {
+    let (r, output) =
+        invoke_and_capture_output(compiled_test.wasm_path(), None, "test-extname", &[]).await;
     let r = r?;
     println!("Output:\n{}", output);
     assert_eq!(r, Some(Val::Bool(true)));
@@ -1270,8 +1279,11 @@ async fn path_test_extname(#[tagged_as("path")] compiled_test: &CompiledTest) ->
 }
 
 #[test]
-async fn path_test_is_absolute(#[tagged_as("path")] compiled_test: &CompiledTest) -> anyhow::Result<()> {
-    let (r, output) = invoke_and_capture_output(compiled_test.wasm_path(), None, "test-is-absolute", &[]).await;
+async fn path_test_is_absolute(
+    #[tagged_as("path")] compiled_test: &CompiledTest,
+) -> anyhow::Result<()> {
+    let (r, output) =
+        invoke_and_capture_output(compiled_test.wasm_path(), None, "test-is-absolute", &[]).await;
     let r = r?;
     println!("Output:\n{}", output);
     assert_eq!(r, Some(Val::Bool(true)));
@@ -1280,7 +1292,8 @@ async fn path_test_is_absolute(#[tagged_as("path")] compiled_test: &CompiledTest
 
 #[test]
 async fn path_test_join(#[tagged_as("path")] compiled_test: &CompiledTest) -> anyhow::Result<()> {
-    let (r, output) = invoke_and_capture_output(compiled_test.wasm_path(), None, "test-join", &[]).await;
+    let (r, output) =
+        invoke_and_capture_output(compiled_test.wasm_path(), None, "test-join", &[]).await;
     let r = r?;
     println!("Output:\n{}", output);
     assert_eq!(r, Some(Val::Bool(true)));
@@ -1288,8 +1301,11 @@ async fn path_test_join(#[tagged_as("path")] compiled_test: &CompiledTest) -> an
 }
 
 #[test]
-async fn path_test_normalize(#[tagged_as("path")] compiled_test: &CompiledTest) -> anyhow::Result<()> {
-    let (r, output) = invoke_and_capture_output(compiled_test.wasm_path(), None, "test-normalize", &[]).await;
+async fn path_test_normalize(
+    #[tagged_as("path")] compiled_test: &CompiledTest,
+) -> anyhow::Result<()> {
+    let (r, output) =
+        invoke_and_capture_output(compiled_test.wasm_path(), None, "test-normalize", &[]).await;
     let r = r?;
     println!("Output:\n{}", output);
     assert_eq!(r, Some(Val::Bool(true)));
@@ -1297,8 +1313,11 @@ async fn path_test_normalize(#[tagged_as("path")] compiled_test: &CompiledTest) 
 }
 
 #[test]
-async fn path_test_relative(#[tagged_as("path")] compiled_test: &CompiledTest) -> anyhow::Result<()> {
-    let (r, output) = invoke_and_capture_output(compiled_test.wasm_path(), None, "test-relative", &[]).await;
+async fn path_test_relative(
+    #[tagged_as("path")] compiled_test: &CompiledTest,
+) -> anyhow::Result<()> {
+    let (r, output) =
+        invoke_and_capture_output(compiled_test.wasm_path(), None, "test-relative", &[]).await;
     let r = r?;
     println!("Output:\n{}", output);
     assert_eq!(r, Some(Val::Bool(true)));
@@ -1306,8 +1325,11 @@ async fn path_test_relative(#[tagged_as("path")] compiled_test: &CompiledTest) -
 }
 
 #[test]
-async fn path_test_resolve(#[tagged_as("path")] compiled_test: &CompiledTest) -> anyhow::Result<()> {
-    let (r, output) = invoke_and_capture_output(compiled_test.wasm_path(), None, "test-resolve", &[]).await;
+async fn path_test_resolve(
+    #[tagged_as("path")] compiled_test: &CompiledTest,
+) -> anyhow::Result<()> {
+    let (r, output) =
+        invoke_and_capture_output(compiled_test.wasm_path(), None, "test-resolve", &[]).await;
     let r = r?;
     println!("Output:\n{}", output);
     assert_eq!(r, Some(Val::Bool(true)));
@@ -1315,8 +1337,11 @@ async fn path_test_resolve(#[tagged_as("path")] compiled_test: &CompiledTest) ->
 }
 
 #[test]
-async fn path_test_parse_format(#[tagged_as("path")] compiled_test: &CompiledTest) -> anyhow::Result<()> {
-    let (r, output) = invoke_and_capture_output(compiled_test.wasm_path(), None, "test-parse-format", &[]).await;
+async fn path_test_parse_format(
+    #[tagged_as("path")] compiled_test: &CompiledTest,
+) -> anyhow::Result<()> {
+    let (r, output) =
+        invoke_and_capture_output(compiled_test.wasm_path(), None, "test-parse-format", &[]).await;
     let r = r?;
     println!("Output:\n{}", output);
     assert_eq!(r, Some(Val::Bool(true)));
@@ -1324,8 +1349,11 @@ async fn path_test_parse_format(#[tagged_as("path")] compiled_test: &CompiledTes
 }
 
 #[test]
-async fn path_test_delimiter(#[tagged_as("path")] compiled_test: &CompiledTest) -> anyhow::Result<()> {
-    let (r, output) = invoke_and_capture_output(compiled_test.wasm_path(), None, "test-delimiter", &[]).await;
+async fn path_test_delimiter(
+    #[tagged_as("path")] compiled_test: &CompiledTest,
+) -> anyhow::Result<()> {
+    let (r, output) =
+        invoke_and_capture_output(compiled_test.wasm_path(), None, "test-delimiter", &[]).await;
     let r = r?;
     println!("Output:\n{}", output);
     assert_eq!(r, Some(Val::Bool(true)));
@@ -1334,10 +1362,10 @@ async fn path_test_delimiter(#[tagged_as("path")] compiled_test: &CompiledTest) 
 
 #[test]
 async fn path_test_sep(#[tagged_as("path")] compiled_test: &CompiledTest) -> anyhow::Result<()> {
-    let (r, output) = invoke_and_capture_output(compiled_test.wasm_path(), None, "test-sep", &[]).await;
+    let (r, output) =
+        invoke_and_capture_output(compiled_test.wasm_path(), None, "test-sep", &[]).await;
     let r = r?;
     println!("Output:\n{}", output);
     assert_eq!(r, Some(Val::Bool(true)));
     Ok(())
 }
-
