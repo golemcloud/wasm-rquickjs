c61f5369df36aa487111f2f6620e4bb304b86838
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2025-12-02 16:57:38 -0500

    Revert "Make `env` a property"
    This reverts commit 2d7d2b6856868ec1a86d7749602b5a021605ada6.

--- crates/wasm-rquickjs/skeleton/src/builtin/process.js
+++ crates/wasm-rquickjs/skeleton/src/builtin/process.js
@@ -7,6 +7,7 @@ import {
 
 export let argv = get_args();
 export let argv0 = argv[0];
+export let env = get_env();
 
 export function cwd() {
     return "/";
@@ -16,12 +17,4 @@ export function nextTick(callback, ...args) {
     next_tick(callback, args);
 }
 
-export default {
-    argv,
-    argv0,
-    get env() {
-        return get_env();
-    },
-    cwd,
-    nextTick
-};
+export default {argv, argv0, env, cwd, nextTick};
