2d7d2b6856868ec1a86d7749602b5a021605ada6
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2025-12-02 15:16:58 -0500

    Make `env` a property

--- crates/wasm-rquickjs/skeleton/src/builtin/process.js
+++ crates/wasm-rquickjs/skeleton/src/builtin/process.js
@@ -7,7 +7,6 @@ import {
 
 export let argv = get_args();
 export let argv0 = argv[0];
-export let env = get_env();
 
 export function cwd() {
     return "/";
@@ -17,4 +16,12 @@ export function nextTick(callback, ...args) {
     next_tick(callback, args);
 }
 
-export default {argv, argv0, env, cwd, nextTick};
+export default {
+    argv,
+    argv0,
+    get env() {
+        return get_env();
+    },
+    cwd,
+    nextTick
+};
